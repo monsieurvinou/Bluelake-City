9cc08bca1ae521db43c027bade6ccc66c716a16f
Vincent Jacquottin <jacquottinvincent@gmail.com>
2015-02-17 16:54:49 +0100

    First Commit

---
+++ Assets/02_SCRIPTS/Camera/CameraFollow.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;            // The position that that camera will be following.
+	public float smoothing = 5f;        // The speed with which the camera will be following.
+	bool isRotating = false;
+	
+	Vector3 offset;                     // The initial offset from the target.
+	
+	void Start ()
+	{
+		// Calculate the initial offset.
+		offset = transform.position - target.position;
+	}
+	
+	void FixedUpdate ()
+	{
+		// Create a postion the camera is aiming for based on the offset from the target.
+		Vector3 targetCamPos = target.position + offset;
+		
+		// Smoothly interpolate between the camera's current position and it's target position.
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+
+		if ( !isRotating && Input.GetAxisRaw("Rotate") != 0 ) {
+			isRotating = true;
+			float rotationToDo = 90f;
+			if ( Input.GetAxisRaw("Rotate") < 0 ) {
+				rotationToDo *= -1;
+			}
+
+			GameObject world = GameObject.Find("World");
+			Hashtable args = new Hashtable(){
+				{"time", 0.7f},
+				{"amount", new Vector3(0, rotationToDo, 0)},
+				{"oncomplete", "AllowRotation"}, 
+				{"oncompletetarget", gameObject}
+			};
+			iTween.RotateAdd(world, args);
+		}
+	}
+
+	void AllowRotation() {
+		isRotating = false;
+	}
+}
---
+++ Assets/02_SCRIPTS/Dialog/DialogBox.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DialogBox : MonoBehaviour {
+	static DialogBox _instance;
+	public GameObject background;
+	public GameObject text;
+
+	GameObject currentTarget;
+	float currentOffsetX = 0;
+	float currentOffsetY = 0;
+
+	public static DialogBox instance {
+		get {
+			if ( _instance == null ) {
+				_instance = GameObject.FindObjectOfType <DialogBox>();
+			}
+			return _instance;
+		}
+	}
+
+	// Use this for initialization
+	void Awake () {
+		instance.gameObject.SetActive (false); // Desactivate by default
+	}
+
+	public static void CreateDialog(GameObject target, string content, float width, float height, float offsetX=0, float offsetY=0) {
+		instance.gameObject.SetActive (true);
+		Text textComponent = instance.text.GetComponent<Text> ();
+		RectTransform textRect = instance.text.GetComponent<RectTransform>();
+		RectTransform backgroundRect = instance.background.GetComponent<RectTransform> ();
+		textComponent.text = content;
+
+		backgroundRect.sizeDelta = new Vector2 (width, height);
+		textRect.sizeDelta = new Vector2 (width - 10, height - 10);
+
+		RectTransform dialogRect = instance.gameObject.GetComponent<RectTransform> ();
+		Vector3 newPos = GameObject.Find ("Camera").camera.WorldToScreenPoint(target.transform.position);
+		newPos.x += offsetX;
+		newPos.y += offsetY;
+		dialogRect.position = newPos;
+
+		// Define for the update
+		instance.currentTarget = target;
+		instance.currentOffsetX = offsetX;
+		instance.currentOffsetY = offsetY;
+	}
+
+	void Update() {
+		if (gameObject.activeInHierarchy) {
+			if ( currentTarget != null ) {
+				RectTransform dialogRect = instance.gameObject.GetComponent<RectTransform> ();
+				Vector3 newPos = GameObject.Find ("Camera").camera.WorldToScreenPoint(currentTarget.transform.position);
+				newPos.x += currentOffsetX;
+				newPos.y += currentOffsetY;
+				dialogRect.position = newPos;
+			}
+		}
+	}
+}
